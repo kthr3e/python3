2c2fb97840d0e5d091c5faec3cffdf9efe59b532
kthr3e <kimiyuki001@gmail.com>
2020-07-23 13:18:48 +0900

    あ

--- 大会やテスト/お見合い.py
+++ 大会やテスト/お見合い.py
@@ -1,4 +1,3 @@
-
 #https://www.suzu6.net/posts/88-python-max-min/
 #https://juppy.hatenablog.com/entry/2018/12/11/python_%E6%96%87%E5%AD%97%E3%82%A2%E3%83%AB%E3%83%95%E3%82%A1%E3%83%99%E3%83%83%E3%83%88%E2%87%86%E6%95%B0%E5%AD%97
 import itertools
